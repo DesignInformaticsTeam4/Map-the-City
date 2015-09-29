8d9ec20518b2f156bc543f6af96eab5fbc1b4ef3
Alex Kearney <konga.kearney@gmail.com>
2015-09-29 19:57:51 +0100

    starting work on new prototype

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,21 +5,21 @@ google.maps.event.addDomListener(window, 'load', initialize);
 function initialize() {
 
   /* position Amsterdam */
-  var latlng = new google.maps.LatLng(52.3731, 4.8922);
+  var latlng = new google.maps.LatLng(55.953251, -3.188267);
 
   var mapOptions = {
     center: latlng,
     scrollWheel: false,
     zoom: 13
   };
-  
+
   var marker = new google.maps.Marker({
     position: latlng,
     url: '/',
     animation: google.maps.Animation.DROP
   });
-  
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
   marker.setMap(map);
 
 };
