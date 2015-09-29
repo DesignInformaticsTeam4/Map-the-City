2c80c70a05d5e42098f2c1f71d0c91c07a203f6d
Alex Kearney <konga.kearney@gmail.com>
2015-09-29 16:52:43 +0100

    added some progress towards integrating a map. Adding bootstrap and JS stuff.

--- mapapp.py
+++ mapapp.py
@@ -10,13 +10,14 @@ DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-# app.config['STATIC_FOLDER'] = os.getcwd()
+app.config['STATIC_FOLDER'] = os.getcwd()
 cfg = None
 
 
 @app.route('/')
 def index():
     return render_template('stuff.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
