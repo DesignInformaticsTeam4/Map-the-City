49df830a6d744236fe03c5a99c4c2e3fe1b8a2db
Alex Kearney <konga.kearney@gmail.com>
2015-09-29 15:37:21 +0100

    added a template project for a tiny demo

---
+++ mapapp.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, request, session, render_template
+import os
+
+__author__ = 'kongaloosh'
+
+# configuration
+DATABASE = 'kongaloosh.db'
+DEBUG = True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+# app.config['STATIC_FOLDER'] = os.getcwd()
+cfg = None
+
+
+@app.route('/')
+def index():
+    return render_template('stuff.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
