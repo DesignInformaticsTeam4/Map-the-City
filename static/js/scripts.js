0f841ab72e8de3c38b9d297a1434ecc38e120868
Alex Kearney <konga.kearney@gmail.com>
2015-09-30 14:50:06 +0100

    itteratively define markers

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,17 +2,6 @@
 $(document).ready(function(){/* google maps -----------------------------------------------------*/
 google.maps.event.addDomListener(window, 'load', initialize);
 
-
-// Animations
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-}
-
-
 function initialize() {                                                       // initialize the map
   var latlng = new google.maps.LatLng(55.953251, -3.188267);                  // set map to edinburgh
 
@@ -22,27 +11,36 @@ function initialize() {                                                       //
     zoom: 13
   };
 
-  var marker = new google.maps.Marker({                                       // add central marker
-    position: latlng,
-    url: '/',
-    animation: google.maps.Animation.DROP
-  });
+//  var marker = new google.maps.Marker({                                       // add central marker
+//    position: latlng,
+//    url: '/',
+//    animation: google.maps.Animation.DROP
+//  });
 
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);  // map
-  marker.setMap(map);
-  //
-  $.getJSON('/json-data/', function(data) {                                   // request the json data from flask app
+  $.getJSON('/json-data/', function(data) {                                      // request the json data from flask app
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);     // map
+        var markers = [];
+
         for (i=0; i < data.length; i++){
-          var myLatLng = {lat: data[i].latitude, lng: data[i].longitude};     // add the lat and lang of a json loc
-          var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: data[i].title                                              // get the memory title
-          });
-          marker.addListener('click', toggleBounce);                          // add animation listener
-        }
-      });
-};
+            var myLatLng = {lat: data[i].latitude, lng: data[i].longitude};      // add the lat and lang of a json loc
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: data[i].title                                              // get the memory title
+            });                                                                   // end making marker
+
+            markers.push(marker);
+            markers[i].setMap(map);
+
+            function toggleBounce() {
+                alert(marker.title);
+            }
+
+            marker.addListener('click', toggleBounce);                          // add animation listener
+        }   // end for
+    }); // end marker adding
+};  // end ajax
 
 /* end google maps -----------------------------------------------------*/
 });
