aad40d28cb37a52ad945dbf041126d4abaa7a991
Alex Kearney <konga.kearney@gmail.com>
2015-10-30 16:34:40 +0000

    slight cleanup, nothing of importance

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,69 +1,64 @@
 
 $(document).ready(function(){/* google maps -----------------------------------------------------*/
-google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
 
-function highlight_trigger(id) {
-    var div = document.getElementById(id);
-    div.style.backgroundColor='#EBEBEB';
-}
+    function highlight_trigger(id) {
+        var div = document.getElementById(id);
+        div.style.backgroundColor='#EBEBEB';
+    }
 
-navigator.geolocation.getCurrentPosition(GetLocation);
-/*
-    gives us...
-    location.coords.latitude
-    location.coords.longitude
-    location.coords.accuracy
+    var location = navigator.geolocation.getCurrentPosition();
+    alert(location.coords.latitude)
+    console.log('here')
+    /*
+        gives us...
+        location.coords.latitude
+        location.coords.longitude
+        location.coords.accuracy
+    */
 
+    function initialize() {                                                       // initialize the map
+        var latlng = new google.maps.LatLng(55.953251, -3.188267);                // set map to edinburgh
+//        console.log('map initialized to ')
+        var mapOptions = {                                                        // setting up the map
+            center: latlng,
+            scrollWheel: false,
+            zoom: 13
+        };
 
-*/
+    //    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    //    var GeoMarker = new GeolocationMarker(map);
 
-function initialize() {                                                       // initialize the map
-  var latlng = new google.maps.LatLng(55.953251, -3.188267);                  // set map to edinburgh
+        $.getJSON('/json-data/', function(data) {                                           // request the json data from flask app
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);      // map
+            var markers = [];
 
-  var mapOptions = {
-    center: latlng,
-    scrollWheel: false,
-    zoom: 13
-  };
+            for (i=0; i < data.length; i++){
+                var myLatLng = {lat: data[i].latitude, lng: data[i].longitude};      // add the lat and lang of a json loc
 
-//    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-//    var GeoMarker = new GeolocationMarker(map);
+                var marker = new google.maps.Marker({
+                    position: myLatLng,
+                    map: map,
+                    title: data[i].title                                              // get the memory title
+                });                                                                   // end making marker
 
-  $.getJSON('/json-data/', function(data) {                                             // request the json data from flask app
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);      // map
-        var markers = [];
+                marker.id = data[i].id
+                markers.push(marker);
+                marker.setMap(map);
+                var id = data[i].id
+                marker.addListener('click', function(){
+    //                highlight_trigger(marker.id)
+                    var div = document.getElementById(marker.id);
+                    div.style.backgroundColor='#EBEBEB';
 
-        for (i=0; i < data.length; i++){
-            var myLatLng = {lat: data[i].latitude, lng: data[i].longitude};      // add the lat and lang of a json loc
+                    });
 
-            var marker = new google.maps.Marker({
-                position: myLatLng,
-                map: map,
-                title: data[i].title                                              // get the memory title
-            });                                                                   // end making marker
-
-            marker.id = data[i].id
-            markers.push(marker);
-            marker.setMap(map);
-            var id = data[i].id
-            marker.addListener('click', function(){
-//                highlight_trigger(marker.id)
-                var div = document.getElementById(marker.id);
-                div.style.backgroundColor='#EBEBEB';
-
-            });
-
-
-
-
-
-//            markers[i].addListener('click', toggleBounce(markers[i]) {
-//
-//
-//            });                          // add animation listener
-        }   // end for
-    }); // end marker adding
-};  // end ajax
-
+    //            markers[i].addListener('click', toggleBounce(markers[i]) {
+    //
+    //
+    //            });                          // add animation listener
+                }   // end for
+        }); // end marker adding
+    };  // end ajax
 /* end google maps -----------------------------------------------------*/
 });
