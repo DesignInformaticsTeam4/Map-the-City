09ac0b1bfcb1282de4868930bdf31624f00d6a33
Alex Kearney <konga.kearney@gmail.com>
2015-10-31 16:31:07 +0000

    working on facebook and twitter based authentication

--- mapapp.py
+++ mapapp.py
@@ -1,5 +1,5 @@
-
-from flask import Flask, request, session, render_template
+from flask_oauth import OAuth
+from flask import Flask, request, session, render_template, redirect, url_for
 import os
 import json
 
@@ -14,6 +14,26 @@ app.config.from_object(__name__)
 app.config['STATIC_FOLDER'] = os.getcwd()
 cfg = None
 
+oauth = OAuth()
+
+twitter = oauth.remote_app('twitter',
+    base_url='https://api.twitter.com/1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key='<your key here>',
+    consumer_secret='<your secret here>'
+)
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ('email, ')}
+)
 
 @app.route('/')
 def index():
@@ -27,6 +47,42 @@ def index():
 @app.route('/json-data/')
 def json_data():
         return open('static/data/memories.json').read()
+
+
+@facebook.tokengetter
+def get_facebook_token():
+    return session.get('facebook_token')
+
+
+def pop_login_session():
+    session.pop('logged_in', None)
+    session.pop('facebook_token', None)
+
+
+@app.route("/facebook_login")
+def facebook_login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next'), _external=True))
+
+
+@app.route("/facebook_authorized")
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None or 'access_token' not in resp:
+        return redirect(next_url)
+
+    session['logged_in'] = True
+    session['facebook_token'] = (resp['access_token'], '')
+
+    return redirect(next_url)
+
+
+@app.route("/logout")
+def logout():
+    pop_login_session()
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
