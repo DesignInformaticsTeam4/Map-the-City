51dee4f83a6de16846e6594d222493af85f7e9f5
Alex Kearney <konga.kearney@gmail.com>
2015-11-11 16:47:16 +0000

    added the geo-json data back in from before

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,6 @@
 var map;
+var geo_marker;
+var markers = [];
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -13,9 +15,31 @@ function makeMap(position){
         center: {lat: position.coords.latitude, lng: position.coords.longitude},
         zoom: 15
     });
-    var GeoMarker = new GeolocationMarker(map);
+    geo_marker = new GeolocationMarker(map);
+
+    add_point();
 }
 
 function initMap() {                    // this gets called by the thingy
    getLocation()
 }
+
+function add_point(){
+    $.getJSON('/json-data/', function(data) {                                             // request the json data from flask app
+        for (i=0; i < data.length; i++){
+            var myLatLng = {lat: data[i].latitude, lng: data[i].longitude};      // add the lat and lang of a json loc
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: data[i].title                                              // get the memory title
+            });                                                                   // end making marker
+
+            marker.id = data[i].id
+            markers.push(marker);
+            marker.setMap(map);
+            var id = data[i].id
+        } // end for
+    }); // end marker adding ajax
+}
+
