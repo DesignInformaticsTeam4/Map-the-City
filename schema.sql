256b2a414b7488ed3b689229f572ea1bdb6b9db2
Alex Kearney <konga.kearney@gmail.com>
2015-11-12 10:44:16 +0000

    changes to schema

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
 drop table if exists users;
 create table users (
   id integer primary key autoincrement,
-  user_name text not null
+  user_name text unique not null
 );
 
 drop table if exists progression;
 create table progression(
     id integer primary key autoincrement,
-    user_name text,
-    story_name text,
-    point_number integer,
+    user_name text not null,
+    story_name text not null,
+    point_number integer default 0,
     FOREIGN KEY (user_name) REFERENCES users(user_name)
 );
