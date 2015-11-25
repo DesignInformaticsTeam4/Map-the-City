2492ad3d52e5d8fca78f79002c29e3720becfaf3
Alex Kearney <konga.kearney@gmail.com>
2015-11-25 10:50:36 +0000

    pop-up with story details

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,7 @@
 var map;
 var geo_marker;
 var markers = [];
+var next_point;
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -16,16 +17,15 @@ function makeMap(position){
         zoom: 15
     });
     geo_marker = new GeolocationMarker(map);
-
     add_points();
 }
 
-function initMap() {                                               // this gets called by the thingy
+function initMap() {                                                // this gets called by the thingy
    getLocation()
 }
 
 function add_point(lat_lng, is_grey){
-    var myLatLng = {lat: data.latitude, lng: data.longitude};     // add the lat and lang of a json loc
+    var myLatLng = {lat: data.latitude, lng: data.longitude};       // add the lat and lang of a json loc
 
     if (is_grey){
             var marker = new google.maps.Marker({
@@ -40,43 +40,40 @@ function add_point(lat_lng, is_grey){
                 title: data.title                                  // get the memory title
             });
     }
-
     marker.id = data[i].id
     markers.push(marker);
     marker.setMap(map);
     var id = data[i].id
-
 }
 
 
 function add_points(){
-    $.getJSON('/json-data/', function(data) {                     // request the json data from flask app
+    $.getJSON('/json-data/', function(data) {                                   // request the json data from flask app
 
         var myLatLng = {lat: data.next_point.latitude, lng: data.next_point.longitude};
         var marker = new google.maps.Marker({
                 position: myLatLng,
                 map: map,
-                title: data.next_point.title                                              // get the memory title
+                title: data.next_point.title                                        // get the memory title
         });
         alert(marker.position);
         marker.id = data.next_point.id;
         marker.setMap(map);
         markers.push(marker);
         var id = data.next_point.id;
-
+        next_point = data.next_point
 //        Add Hidden Points
         data = data.hidden_points;
         var grey = 'static/img/grey-marker.png';
         for (i=0; i < data.length; i++){
-            var myLatLng = {lat: data[i].latitude, lng: data[i].longitude};      // add the lat and lang of a json loc
+            var myLatLng = {lat: data[i].latitude, lng: data[i].longitude};         // add the lat and lang of a json loc
 
             var marker = new google.maps.Marker({
                 position: myLatLng,
                 map: map,
-                title: data[i].title ,                                             // get the memory title
+                title: data[i].title ,                                              // get the memory title
                 icon: grey
-            });                                                                   // end making marker
-
+            });                                                                     // end making marker
             marker.id = data[i].id;
             markers.push(marker);
             marker.setMap(map);
@@ -86,5 +83,9 @@ function add_points(){
 }
 
 function checkin(){
+    
+    alert(Object.keys(next_point));
+    alert(next_point.description);
+    alert(next_point.title);
 }
 
